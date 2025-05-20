c8f72f084696c56bb2dbf04a865aee9b9eae079b
To Duy Tuong <tuongtd2@adflex.vn>
2025-05-20 20:17:40 +0700

    init

--- Main.java
+++ Main.java
@@ -1,8 +1,11 @@
 //import Test7.Solution;
 
+import Test151.Solution;
 import Test21.ListNode;
-import Test93.Solution;
+//import Test93.Solution;
+
 //import Test21.Solution;
+
 
 public class Main {
     public static void main(String[] args) {
@@ -10,7 +13,7 @@ public class Main {
 //        int reverse = solution.reverse(-123);
 //        System.out.println(reverse);
         Solution solution = new Solution();
-        System.out.println(solution.restoreIpAddresses("25525511135"));
+        System.out.println(solution.reverseWords("the sky is blue"));
 
 //        ListNode list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
 //        ListNode list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
--- Test151/Solution.java
+++ Test151/Solution.java
@@ -1,4 +1,26 @@
 package Test151;
 
+import java.util.Stack;
+
 public class Solution {
+    public String reverseWords(String s) {
+        StringBuilder sb = new StringBuilder();
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) != ' ') {
+                stack.push(s.charAt(i));
+            } else if (!stack.empty()) {
+                while (!stack.empty()) {
+                    sb.append(stack.pop());
+                }
+                sb.append(" ");
+            }
+        }
+        while (!stack.empty()) {
+            sb.append(stack.pop());
+        }
+
+        return sb.toString().trim();
+    }
 }
